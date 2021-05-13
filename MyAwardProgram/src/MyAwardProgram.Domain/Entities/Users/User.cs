d24694467be9ef60b44df4778a0eeffd5501c38a
André Bertoletti <abertoletti@luxfacta.com>
2021-05-13 20:04:10 -0300

    Adding Password property

--- MyAwardProgram/src/MyAwardProgram.Domain/Entities/Users/User.cs
+++ MyAwardProgram/src/MyAwardProgram.Domain/Entities/Users/User.cs
@@ -11,6 +11,7 @@ namespace MyAwardProgram.Domain.Entities.Users
         public string Email { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
+        public string Password { get; set; }
 
         public ICollection<UserAddress> Addresses { get; set; }
         public ICollection<Movement> Movements { get; set; }
