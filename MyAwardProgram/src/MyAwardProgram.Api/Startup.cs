420b5635dd8cfc5add6d996711690efad3d71dbd
André Bertoletti <abertoletti@luxfacta.com>
2021-05-12 21:22:47 -0300

    Removing template examples

--- MyAwardProgram/src/MyAwardProgram.Api/Startup.cs
+++ MyAwardProgram/src/MyAwardProgram.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace MyAwardProgram.Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiNet5", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Award Program API", Version = "v1" });
             });
         }
 
@@ -40,7 +40,7 @@ namespace MyAwardProgram.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiNet5 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAwardProgramApi"));
             }
 
             app.UseHttpsRedirection();
